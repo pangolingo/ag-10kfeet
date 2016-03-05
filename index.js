25520b6cef8b07e16ac0245bfb2a270723409696
Dave Iverson <dave@ivrsn.com>
2016-03-05 15:19:28 -0600

    removed unneeded module. cleaned up code. made grouping by Task optional

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const TenKAPI = require('./api');
 
 const startDate = '2016-02-21T00:00:00Z';
 const endDate = '2016-02-27T00:00:00Z';
+const showTasks = false;
 let projects;
 
 function filterDevUsers(users){
@@ -22,27 +23,12 @@ function findProjectById(projects, id){
 }
 
 function displayTimeEntry(projects, entry){
-  // let project = findProjectById(projects, entry.assignable_id);
-  // let projectName = 'UNKNOWN PROJECT';
-  // let phaseName = 'NO PHASE'
-  // if(project){
-  //   projectName = project.name;
-  // }
-  // if(project && project.parent_id){
-  //   phaseName = project.phase_name
-  // }
   // return `${projectName} // ${phaseName} // ${entry.task || 'NO TASK'} -- ${entry.notes} -- ${entry.hours}` 
-  return `${entry.project_name} // ${entry.phase_name} // ${entry.task || 'NO TASK'} -- ${entry.hours}` 
-}
-
-// for some reason, node's findIndex array method doesn't pass in an index.
-function findMatchAndReturnIndex(arr, matchFunction){
-  for (let i = 0; i < arr.length; i++) {
-    if( matchFunction(arr[i], i) ){
-      return i;
-    }
-  }
-  return -1;
+  if(showTasks){
+    return `${entry.project_name} // ${entry.phase_name} // ${entry.task || 'NO TASK'} -- ${entry.hours}`
+  } else {
+    return `${entry.project_name} // ${entry.phase_name} -- ${entry.hours}`
+  } 
 }
 
 
@@ -66,26 +52,29 @@ function addProjectInfoToEntries(entries){
 
 // group time entries with the same project, phase and task
 function groupSimilarEntries(entries){
-  
-  let groupedEntries = [];
-  // for(let entry in entries){
+  let uniqueEntries = [];
   for(let i = 0; i < entries.length; i++){
     let entry = entries[i];
-    let similarGroupedEntryIndex = findMatchAndReturnIndex(groupedEntries, function(groupedEntry, index){
+    let similarEntryIndex = uniqueEntries.findIndex(function(groupedEntry){
       return entriesAreSimilar(entry, groupedEntry)
     });
-    if(similarGroupedEntryIndex > -1){
-      // append hours
-      groupedEntries[similarGroupedEntryIndex].hours += entry.hours;
+    if(similarEntryIndex < 0){
+      // we don't have a similar entry yet
+      uniqueEntries.push(entry);
     } else {
-      console.log("pushing")
-      groupedEntries.push(entry);
+      // we have a similar entry. append hours
+      uniqueEntries[similarEntryIndex].hours += entry.hours;
     }
   }
-  return groupedEntries;
+  return uniqueEntries;
 }
 function entriesAreSimilar(entry1, entry2){
-  const keysToCheck = ['project_name', 'phase_name', 'task'];
+  let keysToCheck;
+  if(showTasks){
+    keysToCheck = ['project_name', 'phase_name', 'task'];
+  } else {
+    keysToCheck = ['project_name', 'phase_name'];
+  }
   for(let i = 0; i < keysToCheck.length; i++){
     let key = keysToCheck[i];
     if(entry1[key] !== entry2[key]){
@@ -98,7 +87,7 @@ function entriesAreSimilar(entry1, entry2){
 
 function filterUsers(users){
   users = filterDevUsers(users);
-  users = users.filter(function(user){ return user.display_name === "Dave Iverson"; });
+  // users = users.filter(function(user){ return user.display_name === "Dave Iverson"; });
   return users;
 }
 
@@ -114,11 +103,17 @@ function filterTimeEntries(entries){
   });
 }
 function displayTimeEntries(user, entries){
-  console.log(user.display_name, `has ${entries.length} entries`);
+  let totalTime = countTotalTime(entries);
+  console.log(user.display_name, `has ${entries.length} entries (${totalTime} hours)`);
     console.log( entries.map(function(entry){ 
       return displayTimeEntry(projects, entry);
     }) );
   return entries;
+}
+function countTotalTime(entries){
+  return entries.reduce(function(total, entry){
+    return total + entry.hours;
+  }, 0);
 }
 
 
